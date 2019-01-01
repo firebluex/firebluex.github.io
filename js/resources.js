5f9b63f5b8fb68579cb077b4dbebe2f153fdc606
firebluex <fireblue@o2.pl>
2019-01-01 14:42:27 +0100

    add files

---
+++ js/resources.js
@@ -0,0 +1,222 @@
+game.resources = [
+    {
+        "name": "dst-inertexponent",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "cling",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "jump",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "stomp",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "erodate",
+        "type": "image",
+        "src": "data/img/erodate.png"
+    },
+    {
+        "name": "fireads",
+        "type": "image",
+        "src": "data/img/fireads.png"
+    },
+    {
+        "name": "flirt",
+        "type": "image",
+        "src": "data/img/flirt.png"
+    },
+    {
+        "name": "gameover",
+        "type": "image",
+        "src": "data/img/gameover.png"
+    },
+    {
+        "name": "gray_bkg",
+        "type": "image",
+        "src": "data/img/gray_bkg.png"
+    },
+    {
+        "name": "leadnotify",
+        "type": "image",
+        "src": "data/img/gui/leadnotify.png"
+    },
+    {
+        "name": "idylla1",
+        "type": "image",
+        "src": "data/img/idylla1.png"
+    },
+    {
+        "name": "idylla2",
+        "type": "image",
+        "src": "data/img/idylla2.png"
+    },
+    {
+        "name": "idylla3",
+        "type": "image",
+        "src": "data/img/idylla3.png"
+    },
+    {
+        "name": "idylla4",
+        "type": "image",
+        "src": "data/img/idylla4.png"
+    },
+    {
+        "name": "idylla5",
+        "type": "image",
+        "src": "data/img/idylla5.png"
+    },
+    {
+        "name": "idylla6",
+        "type": "image",
+        "src": "data/img/idylla6.png"
+    },
+    {
+        "name": "idylla7",
+        "type": "image",
+        "src": "data/img/idylla7.png"
+    },
+    {
+        "name": "0x72_16x16RobotTileset.v1",
+        "type": "image",
+        "src": "data/img/map/0x72_16x16RobotTileset.v1.png"
+    },
+    {
+        "name": "area01_level_tiles",
+        "type": "image",
+        "src": "data/img/map/area01_level_tiles.png"
+    },
+    {
+        "name": "scifi_platformTiles_32x32",
+        "type": "image",
+        "src": "data/img/map/scifi_platformTiles_32x32.png"
+    },
+    {
+        "name": "money",
+        "type": "image",
+        "src": "data/img/money.png"
+    },
+    {
+        "name": "polana1",
+        "type": "image",
+        "src": "data/img/polana1.png"
+    },
+    {
+        "name": "polana2",
+        "type": "image",
+        "src": "data/img/polana2.png"
+    },
+    {
+        "name": "polana3",
+        "type": "image",
+        "src": "data/img/polana3.png"
+    },
+    {
+        "name": "enemy",
+        "type": "image",
+        "src": "data/img/sprite/enemy.png"
+    },
+    {
+        "name": "gripe_run_right",
+        "type": "image",
+        "src": "data/img/sprite/gripe_run_right.png"
+    },
+    {
+        "name": "princess",
+        "type": "image",
+        "src": "data/img/sprite/princess.png"
+    },
+    {
+        "name": "princess1",
+        "type": "image",
+        "src": "data/img/sprite/princess1.png"
+    },
+    {
+        "name": "princess2",
+        "type": "image",
+        "src": "data/img/sprite/princess2.png"
+    },
+    {
+        "name": "protoman_run_right",
+        "type": "image",
+        "src": "data/img/sprite/protoman_run_right.png"
+    },
+    {
+        "name": "protoman_run_right2",
+        "type": "image",
+        "src": "data/img/sprite/protoman_run_right2.png"
+    },
+    {
+        "name": "protoman_run_right3",
+        "type": "image",
+        "src": "data/img/sprite/protoman_run_right3.png"
+    },
+    {
+        "name": "spinning_coin_gold",
+        "type": "image",
+        "src": "data/img/sprite/spinning_coin_gold.png"
+    },
+    {
+        "name": "wheelie_right",
+        "type": "image",
+        "src": "data/img/sprite/wheelie_right.png"
+    },
+    {
+        "name": "area01 — kopia",
+        "type": "tmx",
+        "src": "data/map/area01 — kopia.tmx"
+    },
+    {
+        "name": "area01",
+        "type": "tmx",
+        "src": "data/map/area01.tmx"
+    },
+    {
+        "name": "area012",
+        "type": "tmx",
+        "src": "data/map/area012.tmx"
+    },
+    {
+        "name": "area01copy",
+        "type": "tmx",
+        "src": "data/map/area01copy.tmx"
+    },
+    {
+        "name": "area01cp",
+        "type": "tmx",
+        "src": "data/map/area01cp.tmx"
+    },
+    {
+        "name": "area02",
+        "type": "tmx",
+        "src": "data/map/area02.tmx"
+    },
+    {
+        "name": "0x72_16x16RobotTileset.v1",
+        "type": "tsx",
+        "src": "data/map/0x72_16x16RobotTileset.v1.tsx"
+    },
+    {
+        "name": "scifi_platformTiles_32x32",
+        "type": "tsx",
+        "src": "data/map/scifi_platformTiles_32x32.tsx"
+    },
+    {
+        "name": "PressStart2P",
+        "type": "binary",
+        "src": "data/fnt/PressStart2P.fnt"
+    },
+    {
+        "name": "PressStart2P",
+        "type": "image",
+        "src": "data/fnt/PressStart2P.png"
+    }
+];
